37850cd48f4ca1a03eeba953c873e0aba7e6cca4
ruslan.gadelshin <ruslan.gadelshin@paynet.uz>
2019-11-24 17:28:02 +0500

    добавлена логика для класса BishopBlack

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -1,5 +1,6 @@
 package ru.job4j.chess.firuges.black;
 
+import javafx.css.Match;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
@@ -22,27 +23,35 @@ public class BishopBlack implements Figure {
 
     @Override
     public Cell[] way(Cell source, Cell dest) {
-        throw new IllegalStateException(
-                String.format("Could not way by diagonal from %s to %s", source, dest)
-        );
-//        if (!isDiagonal(source, dest)) {
-//            throw new IllegalStateException(
-//                    String.format("Could not way by diagonal from %s to %s", source, dest)
-//            );
-//        }
-//        int size = ...;
-//        Cell[] steps = new Cell[size];
-//        int deltaX = ...;
-//        int deltaY = ...;
-//        for (int index = 0; index < size; index++) {
-//            steps[index] = ...
-//        }
-//        return steps;
+        if (!isDiagonal(source, dest)) {
+            throw new IllegalStateException(
+                    String.format("Could not way by diagonal from %s to %s", source, dest));
+       }
+        int size = Math.abs(source.x - dest.x);//(int)Math.sqrt(Math.pow(source.x - dest.x, 2) +  Math.pow(dest.y - source.y, 2));
+        Cell[] steps = new Cell[size];
+        int deltaX = source.x > dest.x ? -1 : 1;
+        int deltaY = source.y > dest.y ? -1 : 1;
+        int cellsEnumLength = Cell.values().length;
+        for (int index = 0; index < size; index++) {
+            for (int j = 0; j < cellsEnumLength; j++) {
+                int x = source.x + deltaX + index * deltaX;
+                int y = source.y + deltaY + index * deltaY;
+                if ((Cell.values()[j].x == x) && (Cell.values()[j].y == y)){
+                    steps[index] = Cell.values()[j];
+                    break;
+                }
+            }
+
+        }
+        return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        //TODO check diagonal
-        return false;
+        boolean result = false;
+        if (Math.abs(source.x - dest.x) == Math.abs(dest.y - source.y)) {
+            result = true;
+        }
+        return result;
     }
 
     @Override

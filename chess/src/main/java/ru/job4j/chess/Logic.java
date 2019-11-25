275383ce3fc0f71ee80417af2b579cde82f0c497
ruslan.gadelshin <ruslan.gadelshin@paynet.uz>
2019-11-25 15:58:48 +0500

    Revert "добавлена обработка исключения в Logic"
    This reverts commit f6ed17863175f5635f26eafce7e0023b00a55a30.

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -25,15 +25,11 @@ public class Logic {
         boolean rst = false;
         int index = this.findBy(source);
         if (index != -1) {
-            try {
-                Cell[] steps = this.figures[index].way(source, dest);
-                if (steps.length > 0 && steps[steps.length - 1].equals(dest) && roadIsFree(steps)) {
+            Cell[] steps = this.figures[index].way(source, dest);
+            if (steps.length > 0 && steps[steps.length - 1].equals(dest) && roadIsFree(steps)) {
 
-                    rst = true;
-                    this.figures[index] = this.figures[index].copy(dest);
-                }
-            } catch (IllegalStateException exp) {
-
+                rst = true;
+                this.figures[index] = this.figures[index].copy(dest);
             }
         }
         return rst;
